ba85e5931add249c3725c23f3b9c6a115d02eb34
zcjava <zcjava@qq.com>
2024-04-02 22:45:20 +0800

    custom need constant,Facilitate others modify

--- early-init.el
+++ early-init.el
@@ -1,7 +1,12 @@
 ;; GC settings
 (setq gc-cons-threshold most-positive-fixnum)
 (setq read-process-output-max (* 32 1024 1024))
-(add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 800000)))
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	      (setq gc-cons-threshold 800000)))
+
+;; disable package load elpa
+(setq package-enable-at-startup nil)
 
 ;; Menu-bar
 (menu-bar-mode -1)
@@ -11,3 +16,6 @@
 
 ;; scroll-bar
 (scroll-bar-mode -1)
+
+
+(provide 'early-init)
